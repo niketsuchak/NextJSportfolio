ec5e11323d01f18f4e9b10e3665c03290def0b03
niketsuchak <niket.suchak2021@vitstudent.ac.in>
2025-12-05 01:20:41 +0530

    project and about update

--- components/Projects/constants.ts
+++ components/Projects/constants.ts
@@ -2,38 +2,52 @@ import { Project } from './types';
 
 export const projects: Project[] = [
   {
+    title: 'Uber Clone Backend',
+    src: 'uberclone.png',
+    color: '#dbeafe',
+    url: 'https://github.com/niketsuchak/Uber-Springboot',
+    role: 'Backend Project',
+  },
+  {
+    title: 'AI Content Generator',
+    src: 'aicontentgen.png',
+    color: '#dbeafe',
+    url: 'https://github.com/niketsuchak/AI_Content_Generator',
+    role: 'Fullstack Project',
+  },
+  {
     title: 'Newsify',
     src: 'newsify.png',
     color: '#dbeafe',
     url: 'https://github.com/niketsuchak/Newsify',
-    role: 'Fullstack Developer',
+    role: 'Fullstack Project',
   },
   {
     title: 'TFIx Careers Site',
     src: 'tfix.png',
     color: '#dbeafe',
     url: 'https://sites.google.com/teachforindia.org/careerfair/home',
-    role: 'Microsite Developer',
+    role: 'Microsite',
   },
   {
     title: 'Currency Converter',
     src: 'currencyconverter.png',
     color: '#ddd6fe',
     url: 'https://currencycconverter.netlify.app/',
-    role: 'Frontend Developer',
+    role: 'Frontend Project',
   },
   {
     title: 'Game of Numbers',
     src: 'mathgame.png',
     color: '#ffedd5',
     url: 'https://niketsuchak.github.io/Game_of_numbers/',
-    role: 'Frontend Developer',
+    role: 'Frontend Project',
   },
   {
     title: 'Snakegame',
     src: 'snakegame.png',
     color: '#ecfccb',
     url: 'https://niketsuchak.github.io/Snake_Game//',
-    role: 'Frontend Developer',
+    role: 'Frontend Project',
   },
 ];
